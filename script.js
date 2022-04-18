ba12e0354a4a9479a612dc3b097f3da288fc5905
PocketRice <79682953+PocketRice@users.noreply.github.com>
2022-04-18 18:53:49 -0400

    Added panel persistence
    Fancy way of essentially saying "the panels now won't fade away until you hover over another marker" -- and contains overcomplicated code to implement that too (at least it works!)

--- script.js
+++ script.js
@@ -1,29 +1,64 @@
 window.addEventListener("load", function() {
     var markerS = []; // The markers themselves
     var markerM = []; // The corresponding slide
+    var prevMarker;
+    var hasHovered = false;
+    var emptyMarker = document.getElementsByClassName("emptyMarker");
+
 
     for (let i = 0; i < 20; i++)
     {
         markerS.push(document.getElementById("s"+(i+1)));
         markerM.push(document.getElementById("m"+(i+1)));
-        /*console.log(markerS);
-        console.log(markerM);*/ // DEBUG
-
 
         markerS[i].addEventListener("mouseover", function() {
-
         markerM[i].style.opacity = "1";
+
+// ====== CUT THIS SECTION TO REMOVE PANEL PERSISTENCE =========
+        prevMarker = i;
+
+        for (let z = 0; z < 20; z++)
+        {
+            if (z != i || z != prevMarker)
+            {
+                markerM[z].style.opacity = "0";
+            }
+        }
+
+
+        if (hasHovered == false)
+        {
+            hasHovered = true; // One-time function
+            setTimeout(1000, markerReplace());
+
+        }
+
+// =============================================================
+
 })
 
-        markerS[i].addEventListener("mouseout", function() {
+// ============= UNCOMMENT THIS TO REMOVE PANEL PERSISTENCE ==========
+       /* markerS[i].addEventListener("mouseout", function() {
 
         markerM[i].style.opacity = "0";
-        })
+        })*/
+
+// ===================================================================
+
     }
 
 
-   
+   function markerReplace()
+{
+    emptyMarker[0].style.opacity = 0;
+    emptyMarker[1].style.opacity = 1;
+}
+
+
+
 })
+
+
 
 
 
